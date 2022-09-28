2977c4ada5c449f1f64484768622f3ce5608cc17
mejiayancy18@gmail.com <mejiayancy18@gmail.com>
2022-09-28 21:57:20 +0800

    Update

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -10,7 +10,7 @@ namespace TicTacToe
     {
         public void StartGame() 
         {
-
+            int gameStatus = 0;
             int currentPlayer = -1;
             char[] gameMarker = {'1', '2', '3', '4', '5', '6', '7', '8', '9' };
             
@@ -21,35 +21,75 @@ namespace TicTacToe
                 DrawGameBoard(gameMarker);
                 HeadsUpDisplay(currentPlayer);
                 GameEngine(gameMarker,currentPlayer);
-            } while (true);
+                gameStatus = CheckWinner(gameMarker);
+            } while (gameStatus.Equals(0));
+
+            if (gameStatus.Equals(1)) 
+            {
+                Console.Clear();
+                HeadsUpDisplay(currentPlayer);
+                DrawGameBoard(gameMarker);
+                Console.Write($"Player {currentPlayer} is the winner");
+            }
+            else if (gameStatus.Equals(2)) 
+            {
+                Console.Clear();
+                HeadsUpDisplay(currentPlayer);
+                DrawGameBoard(gameMarker);
+                Console.Write($"The Game is Draw!");
+            }
            
            
         }
 
+        private int CheckWinner(char[] gameMarker)
+        {
+            if (gameMarker[0].Equals(gameMarker[1]) && gameMarker[1].Equals(gameMarker[2])) 
+            {
+                return 1;
+            }
+            return 0;
+        }
+
         private void GameEngine(char[] gameMarker, int currentPlayer)
         {
-            string userInput = Console.ReadLine();
-            if (userInput.Equals("1") || userInput.Equals("2") || userInput.Equals("3") || userInput.Equals("4") || userInput.Equals("5") || userInput.Equals("6") || userInput.Equals("7") || userInput.Equals("8") || userInput.Equals("9"))
+            bool notvalidMove = true;
+
+            do
             {
-                Console.Clear();
-                int.TryParse(userInput, out var gamePlacementMaker);
-                char currentMarker = gameMarker[gamePlacementMaker - 1];
-                if (currentMarker.Equals("X") || currentMarker.Equals("O"))
+                string userInput = Console.ReadLine();
+                if (!string.IsNullOrEmpty(userInput) && (userInput.Equals("1") || userInput.Equals("2") || userInput.Equals("3") || userInput.Equals("4") || userInput.Equals("5") || userInput.Equals("6") || userInput.Equals("7") || userInput.Equals("8") || userInput.Equals("9")))
                 {
-                    Console.WriteLine("Placement has already have a marker. Please select another placement");
+                    Console.Clear();
+                    int.TryParse(userInput, out var gamePlacementMaker);
+                    char currentMarker = gameMarker[gamePlacementMaker - 1];
+                    
+                    if (currentMarker.Equals('X') || currentMarker.Equals('O'))
+                    {
+                        DrawGameBoard(gameMarker);
+                        Console.WriteLine("Placement has already have a marker. Please select another placement");
+                        HeadsUpDisplay(currentPlayer);
+                    }
+                    else
+                    {
+                    
+                        gameMarker[gamePlacementMaker - 1] = GetPlayerMarker(currentPlayer);
+                        notvalidMove = false;
+                    }
                 }
                 else 
                 {
-                    gameMarker[gamePlacementMaker - 1] = GetPlayerMarker(currentPlayer);
+                        Console.WriteLine("Invalid marker. Please select another placement");
                 }
-            }
+            } while (notvalidMove);
+           
         }
 
         public static char GetPlayerMarker(int player) 
         {
             if (player % 2 == 0) 
             {
-                return '0';
+                return 'O';
             }
             return 'X';
         }
